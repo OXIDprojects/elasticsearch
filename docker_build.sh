12302b57bd07e8b1d31416f848146ab4f3212f7a
Tobias Veit <t.veit@syseleven.de>
2018-11-09 13:08:45 +0100

    Dockerfile clean up

--- docker_build.sh
+++ docker_build.sh
@@ -7,5 +7,5 @@ if [[ "$(docker images -q)" != "" ]]; then
 	docker rmi  $(docker images -q)
 fi && \
 docker build -t oxid6 . && \
-docker run -d --name oxid6hackathon  oxid6:latest && \
+docker run -v $PWD/shared/:/var/www/html/ -d --name oxid6hackathon  oxid6:latest && \
 docker exec -ti oxid6hackathon /bin/bash -l
