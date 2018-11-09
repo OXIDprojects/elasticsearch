203e5824cc1d4bb6e76fe24080249958ddb03fef
Tobias Veit <t.veit@syseleven.de>
2018-11-09 12:46:20 +0100

    simple wrapper to start docker instance

---
+++ docker_build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker stop oxid6hackathon
+if [[ "$(docker ps -aq)" != "" ]]; then
+	docker rm $(docker ps -qa)
+fi && \
+if [[ "$(docker images -q)" != "" ]]; then
+	docker rmi  $(docker images -q)
+fi && \
+docker build -t oxid6 . && \
+docker run -e DEBIAN_FRONTEND=noninteractive -d --name oxid6hackathon  oxid6:latest && \
+docker exec -ti oxid6hackathon /bin/bash -l
