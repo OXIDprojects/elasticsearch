ad14f3149f80aa2defd3a59a95e5357bb11ba4f7
Tobias Veit <t.veit@syseleven.de>
2018-11-10 10:07:51 +0100

    add app as submodule https://github.com/OXIDprojects/elasticsearch_module.git

--- shared/app/Application/views/admin/en/oxcomelasticsearch_lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-$sLangName  = "English";
-$aLang = array(
-    'charset'                       	                => 'UTF-8',
-    'OXCOM_ELASTICSEARCH'                             => 'Elasticsearch',
-    'oxcom_elastic_status'                            => 'Status',
-    'oxcom_elastic_cron'                              => 'Cronjobs'
-);
--- shared/app/Cronjob/ArticleResetIndex.php
+++
@@ -1,9 +0,0 @@
-<?php
-// load oxid framework
-require_once(dirname(__FILE__). "/../../../../bootstrap.php");
-$result = \OxidCommunity\Elasticsearch\Application\Controller\Admin\ElasticsearchCron::RecreateArticleIndex();
-if ($result[acknowleged] == '1') {
-    echo 'Done';
-} else {
-    echo $result;
-}
